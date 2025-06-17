f741e86fdf4fd6718d4c3e1c59ac21da68840030
Temurlan01 <erkinbaevtemurlan@gmail.com>
2025-06-17 12:33:53 +0300

    "переделал название эндпоинтов "

--- bank_project/urls.py
+++ bank_project/urls.py
@@ -27,11 +27,11 @@ urlpatterns = [
          name='schema-swagger-ui'),
     path('', lambda request: redirect('/api/v1/users/register/')),
     path('admin/', admin.site.urls),
-    path('api/v1/users/register/', UserRegisterAPIView.as_view()),              # эдпоинт для регистрации
-    path('api/v1/users/login/', UserLoginAPIView.as_view()),       # эдпоинт для логина
-    path('api/v1/user/balance/', UserBalanceAPIView.as_view()),    # эдпоинт для баланса
-    path('api/v1/user/search/', UserSearchAPIView.as_view()),      # эдпоинт для поиска
-    path('api/v1/user/click-button/', ClickButtonAPIView.as_view()),     # эдпоинт для кнопки увеличение баланса
-    path('api/v1/user/send-money/', SendMoneyAPIView.as_view()),         # эдпоинт для оправки денег другому пользователю
-    path('api/v1/user/transactions/', TransactionHistoryAPIView.as_view()),   # эдпоинт для просмотра истории транзакции
+    path('api/v1/users/register/', UserRegisterAPIView.as_view()),             # эдпоинт для регистрации
+    path('api/v1/users/login/', UserLoginAPIView.as_view()),                   # эдпоинт для логина
+    path('api/v1/user/balance-info/', UserBalanceAPIView.as_view()),           # эдпоинт для баланса
+    path('api/v1/user/search-user/', UserSearchAPIView.as_view()),             # эдпоинт для поиска
+    path('api/v1/user/increase-balance/', ClickButtonAPIView.as_view()),       # эдпоинт для кнопки увеличение баланса
+    path('api/v1/user/send-money/', SendMoneyAPIView.as_view()),               # эдпоинт для оправки денег другому пользователю
+    path('api/v1/user/transactions/', TransactionHistoryAPIView.as_view()),    # эдпоинт для просмотра истории транзакции
 ]
